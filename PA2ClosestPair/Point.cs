8cfd8c66de9cad3d70ac9e19e9b0799769f44d02
Edmund Wright <nekurasky@gmail.com>
2018-04-28 21:02:28 -0500

    Can read file, Added Point class
    preparing to write the ClosestPair algo

---
+++ PA2ClosestPair/Point.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PA2ClosestPair
+{
+    public class Point
+    {
+
+        public int x;
+        public int y;
+
+        public double polarAngleProxy;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Distance(Point point)
+        {
+            return Math.Sqrt(Math.Pow(this.x - point.x, 2) + Math.Pow(this.y - point.y, 2));
+        }
+
+        public override string ToString()
+        {
+            return String.Format("( Point x: {0}, y: {1})", x, y);
+        }
+    }
+
+    public class CompareX : IComparer<Point>
+    {
+        public int Compare(Point point1, Point point2)
+        {
+            return point1.x.CompareTo(point2.x);
+        }
+    }
+
+    public class CompareY : IComparer<Point>
+    {
+        public int Compare(Point point1, Point point2)
+        {
+            return point1.y.CompareTo(point2.y);
+        }
+    }
+
+    public class Pair
+    {
+        public Point pointA;
+        public Point pointB;
+        public double distance;
+
+        public Pair(Point pointA, Point pointB, double distance)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
+            this.distance = distance;
+        }
+    }
+}
--- PA2ClosestPair/Program.cs
+++ PA2ClosestPair/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace PA2ClosestPair
 {
@@ -7,6 +11,75 @@ namespace PA2ClosestPair
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            StreamReader file = new StreamReader("10points.txt");
+
+            List<Point> points = new List<Point>();
+
+            string line = null;
+            while ((line = file.ReadLine()) != null)
+            {
+                String[] words = line.Split(" ");
+                Point newPoint = new Point(int.Parse(words[0]), int.Parse(words[1]));
+                points.Add(newPoint);
+                //Debug.WriteLine(line);
+            }
+
+            List<Point> xSorted = points;
+            List<Point> ySorted = new List<Point>(points);
+            ySorted.Sort(new CompareY());
+            xSorted.Sort(new CompareX());
+
+            Debug.WriteLine("Sorted by Y:");
+            foreach (Point point in ySorted)
+            {
+                Debug.WriteLine(point);
+            }
+
+            Debug.WriteLine("Sorted by X:");
+            foreach (Point point in xSorted)
+            {
+                Debug.WriteLine(point);
+            }
+
+
+            // Keep the console window open in debug mode.
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+        }
+
+        public static Pair ClosestPair(List<Point> xSorted, List<Point> ySorted)
+        {
+            Pair closestPair = null;
+            if (xSorted.Count <= 3)
+            {
+                double d1 = xSorted[0].Distance(xSorted[1]);
+                double d2 = xSorted[0].Distance(xSorted[2]);
+                double d3 = xSorted[1].Distance(xSorted[2]);
+
+                if(d1 >= d2 && d1 >= d3)
+                {
+                    closestPair = new Pair(xSorted[0], xSorted[1], d1);
+                }
+                else if (d2 >= d1 && d2 >= d3)
+                {
+                    closestPair = new Pair(xSorted[0], xSorted[2], d2);
+                }
+                else if (d3 >= d1 && d3 >= d2)
+                {
+                    closestPair = new Pair(xSorted[1], xSorted[2], d3);
+                }
+                else
+                {
+                    Debug.Assert(false);
+                }
+            }
+            else
+            {
+
+            }
+
+            return closestPair;
         }
     }
 }
